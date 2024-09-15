5dde547a242e9401d381305601553b4c847e069a
Mayur Hage <125490445+Mayurbariii@users.noreply.github.com>
2024-09-15 15:43:45 +0530

    Added smooth scroll
    Update all pages with smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
 function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
